1beaa3daf7be65fec235c0758c56f58d2ea399f1
Annina <Annina@dhcp-10-176-66-156.dynamic.eduroam.mwn.de>
2014-08-06 18:58:20 +0200

    Homepage steht, Spiel läuft

--- game/states/startScreen.js
+++ game/states/startScreen.js
@@ -6,8 +6,8 @@
   StartScreen.prototype = {
     preload: function() {
       //load images for the startScreen only
-      this.load.image('menu_bg', '././assets/menu1.png');
-      this.load.image('button', '././assets/button_s.png');
+      this.load.image('menu_bg', '././assets/menu/Start/startBildschrim.jpg');
+      this.load.image('button', '././assets/menu/Start/ButtonStart.png');
       this.load.bitmapFont('font_black','././assets/font3.png', 'assets/font.xml');
 
       //load animation of progressbar for the loadingScreen
@@ -19,10 +19,11 @@
       this.menu_bg = this.game.add.sprite(this.game.world.centerX,this.game.world.centerY,'menu_bg');
       this.menu_bg.anchor.setTo(0.5, 0.5);
 
-      this.ok_button = this.game.add.button(this.game.world.centerX,this.game.world.centerY, 'button', this.startClick, this);
+      this.ok_button = this.game.add.button(this.game.world.centerX,this.game.world.centerY+150, 'button', this.startClick, this);
       this.ok_button.anchor.setTo(0.5,0.5);
 
-      this.start_text = this.game.add.bitmapText(this.game.world.centerX-40,this.game.world.centerY-30, 'font_black','Start', 40);
+      this.ok_button.input.useHandCursor = true;
+
     
      
 
